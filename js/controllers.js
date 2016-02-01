33ed74f9e54f7ed403aa04f069df0cc8d29dd13b
Jinsung Lim <js99028@gmail.com>
2016-02-01 23:55:49 +0900

    Merging

--- js/controllers.js
+++ js/controllers.js
@@ -119,7 +119,7 @@ angular.module('app.controllers', [])
 
 .controller('NewsfeedCtrl', function($scope, $state, $ionicPopup, PostModal, AccountService, PostAPI) {
 	AccountService.getUser().then(function(res) {
-		$scope.user = res;
+		$scope.user = AccountService.currentUser;
 	});
 
 	$scope.openPostModal = function() {
@@ -162,12 +162,20 @@ angular.module('app.controllers', [])
 			$scope.posts = $scope.posts.concat(res.objects);
 			if ($scope.posts.length < 1) {
 				$scope.updateFeed();
-			} else if (res.num_results == 0) {
+			} else if (res.num_results == 0 || res.page == res.total_pages) {
 				$scope.canLoadMoreFeed = false;
 			}
 			$scope.$broadcast('scroll.infiniteScrollComplete');
 		});
 	};
+})
+
+.controller('NotificationsCtrl', function($scope, NotificationAPI) {
+	$scope.notifications = [];
+	NotificationAPI.get(function(res) {
+		$scope.notifications = res.objects;
+	});
+})
 
 .controller('PostCtrl', function($scope, $stateParams, $ionicPopup, PostAPI) {
 	$scope.commentData = {};
@@ -175,7 +183,9 @@ angular.module('app.controllers', [])
 	$scope.$on('$ionicView.beforeEnter', function(e) {
 		PostAPI.get({postId: $stateParams.postId}, function(res) {
 			$scope.post = res;
+			$scope.commentData.post_id = res.id;
 		});
+		$scope.updateComment();
 	});
 
 	$scope.doComment = function() {
@@ -220,4 +230,32 @@ angular.module('app.controllers', [])
 			}
 		});
 	};
+
+	$scope.canLoadMoreComment = true;
+	$scope.loadMoreComment = function() {
+		$scope.isLoadingComment = true;
+		PostAPI.getComment({
+			q: {
+				filters: [{
+					name: 'post_id',
+					op: '==',
+					val: $stateParams.postId
+				},{
+					name: 'id',
+					op: '<',
+					val: ($scope.post.comments.length) ? $scope.post.comments[$scope.post.comments.length-1].id : 0
+				}],
+				order_by: [{
+					field: 'id',
+					direction: 'desc'
+				}]
+			}
+		}, function(res) {
+			$scope.post.comments = $scope.post.comments.concat(res.objects);
+			if (res.num_results == 0 || res.page == res.total_pages) {
+				$scope.canLoadMoreComment = false;
+			}
+			$scope.isLoadingComment = false;
+		});
+	}
 })
